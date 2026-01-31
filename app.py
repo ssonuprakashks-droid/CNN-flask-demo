f8d34bccb94e49a427258639797af2e25d4f5cd3
sonu prakash k.s <ssonuprakashks@gmai.com>
2026-01-31 15:06:15 +0530

    five

--- app.py
+++ app.py
@@ -15,5 +15,8 @@ def abc():
     return render_template("abc.html")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
