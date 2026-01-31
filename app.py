20a109c4c2f6283bf8987489bd0f64a9cd3642a1
sonu prakash k.s <ssonuprakashks@gmai.com>
2026-01-31 12:30:13 +0530

    five

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
-@app.route("/inddex")
-def inddex():
-    return render_template("inddex.html")
+
+@app.route("/abc")
+def abc():
+    return render_template("abc.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
